f787260e9cc1051d78c9f9c0f4617b0325bacbad
azertypow <nico@villa1203.ch>
2024-11-26 18:15:26 +0100

    feat: save file in same directory of exec

--- main.ts
+++ main.ts
@@ -1,29 +1,41 @@
 import puppeteer from "npm:puppeteer";
+import * as path from "jsr:@std/path";
+
+
+// Récupérer le chemin de l'exécutable
+// const execPath = import.meta.url;
+// const execDirectoryPath = path.dirname( new URL(execPath).pathname);
+
+const execPath = Deno.execPath();
+const execDirectoryPath = path.dirname( execPath );
+
+console.log( execDirectoryPath )
 
 // URL à capturer et fichier de sortie
 const url = "https://menus.for-pro.ch/foodCourt_screen";
-const outputFilePath = `./screenshot${new Date().getTime()}.png`;
+const outputFilePath = `screenshot${new Date().getTime()}.png`;
 
-(async () => {
-  // Lance un navigateur headless
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+// Lance un navigateur headless
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
 
-  // Charge la page
-  await page.goto(url, { waitUntil: "networkidle0" });
+// Charge la page
+await page.goto(url, { waitUntil: "networkidle0" });
 
-  await page.emulateMediaType('print')
+await page.emulateMediaType('print')
 
-  await page.setViewport({ width: 1920, height: 1080 });
+await page.setViewport({ width: 1920, height: 1080 });
 
-  // Capture d'écran
-  await page.screenshot({
-    path: outputFilePath,
-    // fullPage: true, // Capture toute la page
-  });
+console.log( execDirectoryPath + '/' + outputFilePath )
 
-  console.log(`Image sauvegardée sous ${outputFilePath}`);
+// Capture d'écran
+await page.screenshot({
+  path: execDirectoryPath + '/' + outputFilePath,
+  // fullPage: true, // Capture toute la page
+});
 
-  // Ferme le navigateur
-  await browser.close();
-})();
+console.log(`Image sauvegardée sous ${outputFilePath}`);
+
+// Ferme le navigateur
+await browser.close();
+
